41d579ba3e477df9d05ac6b11846c874e670f74f
Robert Allen <zircote@gmail.com>
2012-12-16 06:44:31 -0600

    Feature enhancement to use `@var` for type
    In the event the `SWG\Property` tag is without the type definition the
    Annotation class with then look to the comment block for a `@var`
    annotation to derive the type of the model property.
    
    Issue: #36

--- tests/Swagger/Fixtures1/Models/Facet.php
+++ tests/Swagger/Fixtures1/Models/Facet.php
@@ -15,4 +15,14 @@ class Facet
      * @SWG\Property(name="prop1", type="string")
      */
     public $prop1;
+    /**
+     * @var string some desc
+     * @SWG\Property
+     */
+    public $prop2;
+    /**
+     * @var Some_Object
+     * @SWG\Property
+     */
+    public $prop3;
 }
