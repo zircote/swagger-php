3d9c756c8b2d414c402dd53c4022644a13ac7d6d
Bob Fanger <bfanger@gmail.com>
2013-09-04 03:58:17 +0200

    Updated Copyright to 2013

--- tests/SwaggerTests/Fixtures2/Models/Dog.php
+++ tests/SwaggerTests/Fixtures2/Models/Dog.php
@@ -3,7 +3,7 @@ namespace SwaggerTests\Fixtures2\Models;
 
 /**
  * @license    http://www.apache.org/licenses/LICENSE-2.0
- *             Copyright [2012] [Robert Allen]
+ *             Copyright [2013] [Robert Allen]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
