68933451cca913e01cf8b323cbf22eb41e855130
Bob Fanger <bfanger@gmail.com>
2015-07-04 19:24:04 +0200

    Added dependency-injection to Swagger\scan()
    Allows a ReflectionAnalyser  #214

--- src/functions.php
+++ src/functions.php
@@ -20,20 +20,27 @@ define('Swagger\Processors\UNDEFINED', UNDEFINED);
  * Scan the filesystem for swagger annotations and build swagger-documentation.
  *
  * @param string|array|Finder $directory The directory(s) or filename(s)
- * @param null|string|array $exclude The directory(s) or filename(s) to exclude (as absolute or relative paths)
+ * @param array $options 
+ *   exclude: string|array $exclude The directory(s) or filename(s) to exclude (as absolute or relative paths)
+ *   analyser: defaults to StaticAnalyser
+ *   analysis: defaults to a new Analysis
+ *   processors: defaults to the registered processors in Analysis
  * @return Swagger
  */
-function scan($directory, $exclude = null)
+function scan($directory, $options = array())
 {
-    $analyser = new StaticAnalyser();
-    $analysis = new Analysis();
+    $analyser = @$options['analyser'] ?: new StaticAnalyser();
+    $analysis = @$options['analysis'] ?: new Analysis();
+    $processors = @$options['processors'] ?: Analysis::processors();
+    $exclude = @$options['exclude'] ?: null;
+
     // Crawl directory and parse all files
     $finder = Util::finder($directory, $exclude);
     foreach ($finder as $file) {
         $analysis->addAnalysis($analyser->fromFile($file->getPathname()));
     }
     // Post processing
-    $analysis->process();
+    $analysis->process($processors);
     // Validation (Generate notices & warnings)
     $analysis->validate();
     return $analysis->swagger;
