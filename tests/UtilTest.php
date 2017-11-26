e10c148b348a4cec12a29ba394842777c49a14cf
Kevin Upton <k_upton@live.com>
2017-11-27 00:53:03 +1000

    updated the exclusions

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -12,7 +12,7 @@ class UtilTest extends SwaggerTestCase
 {
     public function testExclude()
     {
-        $swagger = \Swagger\scan(__DIR__ . '/Fixtures', ['exclude' => ['Customer.php', 'UsingRefs.php', 'UsingPhpDoc.php', 'GrandAncestor.php']]);
+        $swagger = \Swagger\scan(__DIR__ . '/Fixtures', ['exclude' => ['Customer.php', 'UsingRefs.php', 'DynamicReference.php', 'UsingPhpDoc.php', 'GrandAncestor.php']]);
         $this->assertSame('Fixture for ParserTest', $swagger->info->title, 'No errors about duplicate @SWG\Info() annotations');
     }
 }
