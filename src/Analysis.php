d2aa68cbecc5e0ff3193f972ff949af67e155e1e
Ross Williams <ross@solodev.com>
2018-05-15 22:52:32 -0400

    classes with a swagger definition also take their used traits into account when defining properties

--- src/Analysis.php
+++ src/Analysis.php
@@ -168,6 +168,20 @@ class Analysis
         return $definitions;
     }
 
+    public function getUsedTraits($class)
+    {
+        $usedTraits = class_uses($class);
+        $definitions = [];
+        foreach($usedTraits as $usedTrait){
+            $usedTrait = '\\'.$usedTrait;
+            $usedDefinition = isset($this->classes[$usedTrait]) ? $this->classes[$usedTrait] : null;
+            if ($usedDefinition) {
+                $definitions[$usedTrait] = $usedDefinition;
+            }
+        }
+        return $definitions;
+    }
+
     /**
      *
      * @param string $class
