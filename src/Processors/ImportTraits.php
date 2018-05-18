b1743b0fb0f733f21aa7cfef386a31c9dc77b6f0
Gjergj Sheldija <gjergj.sheldija@gmail.com>
2018-05-18 15:51:48 +0200

    Add support for properties imorted from traits

---
+++ src/Processors/ImportTraits.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Swagger\Processors;
+
+use Swagger\Analyser;
+use Swagger\Annotations\Property;
+use Swagger\Annotations\Swagger;
+use Swagger\Annotations\Definition;
+use Swagger\Analysis;
+use Traversable;
+
+
+class ImportTraits
+{
+
+    public function __invoke(Analysis $analysis)
+    {
+        $schemas = $analysis->getAnnotationsOfType('\Swagger\Annotations\Schema');
+        foreach ($schemas as $schema) {
+            $existing = [];
+            if($schema->_context->is('class')) {
+                $traits = $analysis->getTraitsOfClass($schema->_context->fullyQualifiedName($schema->_context->class));
+                foreach ($traits as $trait) {
+                    foreach ($trait['properties'] as $property) {
+                        if (is_array($property->annotations) === false && !($property->annotations instanceof Traversable)) {
+                            continue;
+                        }
+                        foreach ($property->annotations as $annotation) {
+                            if ($annotation instanceof Property && in_array($annotation->property, $existing) === false) {
+                                $existing[] = $annotation->property;
+                                $schema->merge([$annotation], true);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
