b013fd4116ee93ff2989035fd9352f1f2529ec92
Bob Fanger <bfanger@gmail.com>
2018-08-10 15:49:43 +0200

    Correctad array notation in misc example
    Fixes #567

--- Examples/misc/misc-api.php
+++ Examples/misc/misc-api.php
@@ -12,7 +12,7 @@
  *      description="OpenApi parameters",
  *      @OA\ServerVariable(
  *          serverVariable="schema",
- *          enum="['https', 'http']",
+ *          enum={"https", "http"},
  *          default="https"
  *      )
  * )
