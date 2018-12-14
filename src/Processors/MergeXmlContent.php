164708f84ea519f8ae5d1d9c9f09d0c93d4a0a63
Hannu Räbinä <hannu.rabina@probissolutions.fi>
2018-12-14 12:16:51 +0200

    Allow XmlContent in RequestBody

--- src/Processors/MergeXmlContent.php
+++ src/Processors/MergeXmlContent.php
@@ -7,14 +7,14 @@
 namespace OpenApi\Processors;
 
 use OpenApi\Annotations\MediaType;
-use OpenApi\Annotations\JsonContent;
+use OpenApi\Annotations\RequestBody;
 use OpenApi\Annotations\Response;
 use OpenApi\Analysis;
 use OpenApi\Annotations\XmlContent;
 use OpenApi\Context;
 
 /**
- * Split JsonContent into Schema and MediaType
+ * Split XmlContent into Schema and MediaType
  */
 class MergeXmlContent
 {
@@ -23,7 +23,7 @@ class MergeXmlContent
         $annotations = $analysis->getAnnotationsOfType(XmlContent::class);
         foreach ($annotations as $xmlContent) {
             $response = $xmlContent->_context->nested;
-            if (!($response instanceof Response)) {
+            if (!($response instanceof Response) && !($response instanceof RequestBody)) {
                 continue;
             }
             if ($response->content === UNDEFINED) {
