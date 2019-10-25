38e8d705927e47b2e6bb95c421bcd525c8d89a02
bdereta <borisdereta@gmail.com>
2019-10-24 20:12:15 -0700

    Update api.php

--- Examples/petstore-3.0/api.php
+++ Examples/petstore-3.0/api.php
@@ -6,10 +6,7 @@
 
 /**
  * @OA\Info(
- *     description="This is a sample Petstore server.  You can find
-out more about Swagger at
-[http://swagger.io](http://swagger.io) or on
-[irc.freenode.net, #swagger](http://swagger.io/irc/).",
+ *     description="This is a sample Petstore server. You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).",
  *     version="1.0.0",
  *     title="Swagger Petstore",
  *     termsOfService="http://swagger.io/terms/",
