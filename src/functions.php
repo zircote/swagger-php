b41bd1a0ac4b55653940390e794953f0a016dca5
DerManoMann <mano@radebatz.net>
2020-08-18 15:01:29 +1200

    Add support for new PHP8 T_NAME_xxx token
    PHP8 related changes to namespace parsing.

--- src/functions.php
+++ src/functions.php
@@ -18,6 +18,14 @@ if (defined('OpenApi\UNDEFINED') === false) {
     define('OpenApi\Processors\UNDEFINED', UNDEFINED);
 }
 
+if (false === defined('T_NAME_QUALIFIED')) {
+    /*
+     * PHP8 only token.
+     */
+    define('T_NAME_QUALIFIED', -1);
+    define('T_NAME_FULLY_QUALIFIED', -1);
+}
+
 if (function_exists('OpenApi\scan') === false) {
     /**
      * Scan the filesystem for OpenAPI annotations and build openapi-documentation.
