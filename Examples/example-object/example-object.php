a744d9bbfb632552e8d07164afffa94d0e09001a
Martin Rademacher <mano@radebatz.net>
2021-03-03 13:38:38 +1300

    Show how to set multiple examples (#910)
    Fixes #908

--- Examples/example-object/example-object.php
+++ Examples/example-object/example-object.php
@@ -10,6 +10,26 @@ use OpenApi\Annotations as OA;
  */
 
 /**
+ * @OA\Put(
+ *     path="/users/{id}",
+ *     summary="Updates a user",
+ *     @OA\Parameter(
+ *         description="Parameter with mutliple examples",
+ *         in="path",
+ *         name="id",
+ *         required=true,
+ *         @OA\Schema(type="string"),
+ *         @OA\Examples(example="int", value="1", summary="An int value."),
+ *         @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     )
+ * )
+ */
+
+/**
  * @OA\Post(
  *     path="/users",
  *     summary="Adds a new user",
