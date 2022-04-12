9a4fc876ad5f397f58860c3d0dfcce96d2014810
DerManoMann <mano@radebatz.net>
2022-04-12 18:49:24 +1200

    Stick to annotations to make tests more portable

--- tests/Annotations/ComponentsTest.php
+++ tests/Annotations/ComponentsTest.php
@@ -3,8 +3,8 @@
 namespace OpenApi\Tests\Annotations;
 
 use OpenApi\Annotations\Examples;
-use OpenApi\Attributes\Components;
-use OpenApi\Attributes\Schema;
+use OpenApi\Annotations\Components;
+use OpenApi\Annotations\Schema;
 use OpenApi\Tests\OpenApiTestCase;
 
 class ComponentsTest extends OpenApiTestCase
@@ -12,7 +12,7 @@ class ComponentsTest extends OpenApiTestCase
     public function testRef()
     {
         $this->assertEquals('#/components/schemas/foo', Components::ref('foo'));
-        $this->assertEquals('#/components/schemas/bar', Components::ref(new Schema(null, 'bar')));
+        $this->assertEquals('#/components/schemas/bar', Components::ref(new Schema(['ref' => null, 'schema' => 'bar'])));
         $this->assertEquals('#/components/examples/xx', Components::ref(new Examples(['example' => 'xx'])));
     }
 }
