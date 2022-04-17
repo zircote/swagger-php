ce64507ccc3772bd61eeb11465fd5982bacb9040
Martin Rademacher <mano@radebatz.net>
2022-04-17 13:38:33 +1200

    Sync attribute flags and add `\Attribute::TARGET_PARAMETER` (#1206)

--- src/Attributes/Parameter.php
+++ src/Attributes/Parameter.php
@@ -6,7 +6,7 @@
 
 namespace OpenApi\Attributes;
 
-#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::IS_REPEATABLE)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
 class Parameter extends \OpenApi\Annotations\Parameter
 {
     use ParameterTrait;
