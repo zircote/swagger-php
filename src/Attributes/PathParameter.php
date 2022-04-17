e37fb6ddb8e257edeb464bb665aff204615316ac
DerManoMann <mano@radebatz.net>
2022-04-17 12:53:10 +1200

    Sync attribute flags and add `\Attribute::TARGET_PARAMETER`

--- src/Attributes/PathParameter.php
+++ src/Attributes/PathParameter.php
@@ -6,7 +6,7 @@
 
 namespace OpenApi\Attributes;
 
-#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
 class PathParameter extends \OpenApi\Annotations\PathParameter
 {
     use ParameterTrait;
