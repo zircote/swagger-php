26203192ca40438125b04234f18308f0e81ad2db
Martin Rademacher <mano@radebatz.net>
2022-05-24 12:04:29 +1200

    Allow SecurityScheme on methods/properties (#1246)

--- src/Attributes/SecurityScheme.php
+++ src/Attributes/SecurityScheme.php
@@ -8,7 +8,7 @@ namespace OpenApi\Attributes;
 
 use OpenApi\Generator;
 
-#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::IS_REPEATABLE)]
 class SecurityScheme extends \OpenApi\Annotations\SecurityScheme
 {
     /**
