d263ff860b1ac364f3c07007d933ef3b02de88c6
DerManoMann <mano@radebatz.net>
2022-10-20 10:21:13 +1300

    Add new test to verify  single file fixtures

---
+++ tests/Fixtures/Scratch/MergeTraits.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace OpenApi\Tests\Fixtures\Scratch;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(title="API", version="1.0")
+ *
+ * @OA\Get(
+ *     path="/api/endpoint",
+ *     @OA\Response(
+ *         response=200,
+ *         description="successful operation",
+ *         @OA\JsonContent(ref="#/components/schemas/Address")
+ *     )
+ * )
+ */
+class Endpoint
+{
+}
+
+trait HasId
+{
+    /**
+     * @OA\Property(
+     *     format="int64",
+     *     readOnly=true,
+     * )
+     *
+     * @var int
+     */
+    public int $id;
+}
+
+trait HasTimestamps
+{
+    /**
+     * @OA\Property(
+     *     format="date-time",
+     *     type="string",
+     *     readOnly=true,
+     * )
+     */
+    public \DateTime $created_at;
+
+    /**
+     * @OA\Property(
+     *     format="date-time",
+     *     type="string",
+     *     readOnly=true,
+     * )
+     */
+    public \DateTime $updated_at;
+}
+
+abstract class Model
+{
+    use HasId;
+}
+
+/**
+ * @OA\Schema(
+ *     required={"street"},
+ *     @OA\Xml(
+ *         name="Address"
+ *     )
+ * )
+ */
+class Address extends Model
+{
+    use HasTimestamps;
+
+    /** @OA\Property */
+    public string $street;
+}
