072daaabc105279146bba5a68e4b0741dbf232d6
Juan Treminio <jtreminio@gmail.com>
2023-01-12 10:50:50 -0600

    Allow Examples attribute to be repeatable

--- src/Attributes/Examples.php
+++ src/Attributes/Examples.php
@@ -8,7 +8,7 @@ namespace OpenApi\Attributes;
 
 use OpenApi\Generator;
 
-#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::IS_REPEATABLE)]
 class Examples extends \OpenApi\Annotations\Examples
 {
     /**
