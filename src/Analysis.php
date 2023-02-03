e449670473d7ecaff75dff744e8e9be68a79a44c
Martin Rademacher <mano@radebatz.net>
2023-02-03 14:25:00 +1300

    Make schema lookup work for custom schema implementations (#1403)

--- src/Analysis.php
+++ src/Analysis.php
@@ -7,7 +7,6 @@
 namespace OpenApi;
 
 use OpenApi\Annotations as OA;
-use OpenApi\Attributes as OAT;
 use OpenApi\Processors\ProcessorInterface;
 
 /**
@@ -339,7 +338,7 @@ class Analysis
                 $definition = $definitions[$fqdn];
                 if (is_iterable($definition['context']->annotations)) {
                     foreach (array_reverse($definition['context']->annotations) as $annotation) {
-                        if (in_array(get_class($annotation), [OA\Schema::class, OAT\Schema::class]) && !$annotation->_context->is('generated')) {
+                        if ($annotation->isRoot(OA\Schema::class) && !$annotation->_context->is('generated')) {
                             return $annotation;
                         }
                     }
