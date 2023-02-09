ffbd480ddcf27c4a0db0ee4ab7eed2b724631df6
Gustavo Novaro <gnovaro@users.noreply.github.com>
2023-02-09 09:57:06 +0100

    Improve API Info adding some attributes to the example file for php 8.1

--- Examples/using-links-php81/OpenApiSpec.php
+++ Examples/using-links-php81/OpenApiSpec.php
@@ -4,7 +4,15 @@ namespace OpenApi\Examples\UsingLinksPhp81;
 
 use OpenApi\Attributes as OAT;
 
-#[OAT\Info(version: '1.0.0', title: 'Link Example')]
+#[OAT\Info(
+    version: '1.0.0', 
+    description: '', 
+    title: 'Link Example',
+    contact: new OAT\Contact(
+        name: 'Contact Name',
+        email: 'support@example.com'
+    )
+)]
 class OpenApiSpec
 {
 }
