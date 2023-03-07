40a23b089a435e700505411398a7f3812a593fd1
Gustavo Novaro <gnovaro@users.noreply.github.com>
2023-03-07 19:55:08 +0100

    Improve api doc (#1399)

--- Examples/using-links-php81/UsersController.php
+++ Examples/using-links-php81/UsersController.php
@@ -9,8 +9,28 @@ use OpenApi\Attributes as OAT;
  */
 class UsersController
 {
-    #[OAT\Get(path: '/2.0/users/{username}', operationId: 'getUserByName', parameters: [new OAT\Parameter(name: 'username', in: 'path', required: true, schema: new OAT\Schema(type: 'string'))], responses: [new OAT\Response(response: 200, description: 'The User', content: new OAT\JsonContent(ref: '#/components/schemas/user'), links: [new OAT\Link(link: 'userRepositories', ref: '#/components/links/UserRepositories')])])]
-    public function getUserByName($username)
+    #[OAT\Get(
+        path: '/2.0/users/{username}',
+        operationId: 'getUserByName',
+        summary: 'Get user details by username',
+        tags: ['Users'],
+        parameters: [
+            new OAT\Parameter(
+                name: 'username',
+                in: 'path',
+                required: true,
+                schema: new OAT\Schema(type: 'string')
+            )],
+        responses: [
+            new OAT\Response(
+                response: 200,
+                description: 'The User',
+                content: new OAT\JsonContent(ref: '#/components/schemas/user'),
+                links: [new OAT\Link(link: 'userRepositories', ref: '#/components/links/UserRepositories')]
+            ),
+        ]
+    )]
+    public function getUserByName(string $username)
     {
     }
 }
