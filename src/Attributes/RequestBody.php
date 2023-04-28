dbc4a3e8f60a5711ede80727fd023f6fa07602cc
DerManoMann <mano@radebatz.net>
2023-04-28 12:50:08 +1200

    Make `RequestBody` attribute IS_REPEATABLE

--- src/Attributes/RequestBody.php
+++ src/Attributes/RequestBody.php
@@ -9,7 +9,7 @@ namespace OpenApi\Attributes;
 use OpenApi\Generator;
 use OpenApi\Annotations as OA;
 
-#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
 class RequestBody extends OA\RequestBody
 {
     /**
