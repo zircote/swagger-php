efa760d93ab68eed64afdf6a8aab733e1e621605
Yerlen Zhubangaliyev <yerlen@yerlen.com>
2023-06-14 20:22:23 +0600

    allow OpenApi\Attributes\Options attribute target in methods

--- src/Attributes/Options.php
+++ src/Attributes/Options.php
@@ -6,7 +6,7 @@
 
 namespace OpenApi\Attributes;
 
-#[\Attribute(\Attribute::TARGET_CLASS)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 class Options extends \OpenApi\Annotations\Options
 {
     use OperationTrait;
