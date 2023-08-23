e83c438d138a6a7a8f7b1c26ae436bcf84a69d22
Anton Samoylenko <a.samoylenko95@gmail.com>
2023-08-23 17:14:23 +0300

    Add test

---
+++ tests/Fixtures/PHP/MultipleFunctions.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+/**
+ * @license Apache 2.0
+ */
+
+namespace OpenApi\Tests\Fixtures\PHP;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(title="Test", version="1.0")
+ */
+class MultipleFunctions
+{
+    public function first()
+    {
+        $category = new \stdClass();
+        $prefix = '1';
+        $category->name = '1';
+
+        return isset($category->{'name'.$prefix}) && $category->{'name'.$prefix}
+            ? $category->{'name'.$prefix}
+            : $category->name;
+    }
+
+
+    public function second()
+    {
+
+    }
+}
