097b7c6f7391bfd30eccd36a4cbb582a3e738f82
Attila <Attila@attila>
2023-08-31 08:14:00 +0200

    fix cs

--- src/Attributes/AdditionalProperties.php
+++ src/Attributes/AdditionalProperties.php
@@ -12,17 +12,17 @@ use OpenApi\Generator;
 class AdditionalProperties extends \OpenApi\Annotations\AdditionalProperties
 {
     /**
-     * @param string|class-string|object|null                 $ref
-     * @param string[]                                        $required
-     * @param Property[]                                      $properties
-     * @param int|float                                       $maximum
-     * @param int|float                                       $minimum
+     * @param string|class-string|object|null                        $ref
+     * @param string[]                                               $required
+     * @param Property[]                                             $properties
+     * @param int|float                                              $maximum
+     * @param int|float                                              $minimum
      * @param string[]|int[]|float[]|\UnitEnum[]|class-string|bool[] $enum
-     * @param array<Schema|\OpenApi\Annotations\Schema>       $allOf
-     * @param array<Schema|\OpenApi\Annotations\Schema>       $anyOf
-     * @param array<Schema|\OpenApi\Annotations\Schema>       $oneOf
-     * @param array<string,mixed>|null                        $x
-     * @param Attachable[]|null                               $attachables
+     * @param array<Schema|\OpenApi\Annotations\Schema>              $allOf
+     * @param array<Schema|\OpenApi\Annotations\Schema>              $anyOf
+     * @param array<Schema|\OpenApi\Annotations\Schema>              $oneOf
+     * @param array<string,mixed>|null                               $x
+     * @param Attachable[]|null                                      $attachables
      */
     public function __construct(
         // schema
