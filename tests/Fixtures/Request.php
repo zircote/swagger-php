0496b54d8267e99965d0513bd3961743325b55b5
Dmytro Kulyk <dmytro.kulyk@sapient.pro>
2024-01-30 12:29:54 +0200

    [feat] Added resolving RequestBody->request based on class name

---
+++ tests/Fixtures/Request.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+/**
+ * @license Apache 2.0
+ */
+
+// NOTE: this file uses "\r\n" linebreaks on purpose
+namespace OpenApi\Tests\Fixtures;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\RequestBody()
+ */
+class Request
+{
+
+}
