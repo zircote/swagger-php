090221020034430d8fb66d0b35479fa203fbb2ef
DerManoMann <mano@radebatz.net>
2024-06-03 19:50:15 +1200

    Tweak implementation / tests

--- src/Analysers/TokenScanner.php
+++ src/Analysers/TokenScanner.php
@@ -7,16 +7,12 @@
 namespace OpenApi\Analysers;
 
 use PhpParser\Error;
-use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassLike;
-use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Enum_;
 use PhpParser\Node\Stmt\Interface_;
 use PhpParser\Node\Stmt\Namespace_;
-use PhpParser\Node\Stmt\Property;
 use PhpParser\Node\Stmt\Trait_;
-use PhpParser\Node\Stmt\TraitUse;
 use PhpParser\Node\Stmt\Use_;
 use PhpParser\ParserFactory;
 
@@ -41,35 +37,45 @@ class TokenScanner
 
         $result = [];
         foreach ($stmts as $stmt) {
-            //echo 'top: ' . get_class($stmt), PHP_EOL;
+            // echo 'top: ' . get_class($stmt), PHP_EOL;
             if ($stmt instanceof Namespace_) {
-                $namespace = (string)$stmt->name;
+                $namespace = (string) $stmt->name;
 
                 $uses = [];
-                $resolve = function(string $name) use ($namespace, &$uses) {
+                $resolve = function (string $name) use ($namespace, &$uses) {
                     if (array_key_exists($name, $uses)) {
                         return $uses[$name];
                     }
 
-                    return $namespace.'\\'.$name;
+                    return $namespace . '\\' . $name;
+                };
+                $details = function () use (&$uses) {
+                    return [
+                        'uses' => $uses,
+                        'interfaces' => [],
+                        'traits' => [],
+                        'enums' => [],
+                        'methods' => [],
+                        'properties' => [],
+                    ];
                 };
                 foreach ($stmt->stmts as $subStmt) {
-                    //echo 'sub: ' . get_class($subStmt), PHP_EOL;
+                    // echo 'sub: ' . get_class($subStmt), PHP_EOL;
                     switch (get_class($subStmt)) {
                         case Use_::class:
                             $uses += $this->collect_uses($subStmt);
                             break;
                         case Class_::class:
-                            $result += $this->collect_class($subStmt, $uses, $resolve);
+                            $result += $this->collect_class($subStmt, $details(), $resolve);
                             break;
                         case Interface_::class:
-                            $result += $this->collect_interface($subStmt, $uses, $resolve);
+                            $result += $this->collect_interface($subStmt, $details(), $resolve);
                             break;
                         case Trait_::class:
-                            $result += $this->collect_trait($subStmt, $uses, $resolve);
+                            $result += $this->collect_classlike($subStmt, $details(), $resolve);
                             break;
                         case Enum_::class:
-                            $result += $this->collect_enum($subStmt, $uses, $resolve);
+                            $result += $this->collect_classlike($subStmt, $details(), $resolve);
                             break;
                     }
                 }
@@ -84,42 +90,40 @@ class TokenScanner
         $uses = [];
 
         foreach ($stmt->uses as $use) {
-            $uses[(string)$use->getAlias()] = (string)$use->name;
+            $uses[(string) $use->getAlias()] = (string) $use->name;
         }
 
         return $uses;
     }
 
-    protected function collect_classlike(ClassLike $stmt, array $details, callable $resolve):
-    array
+    protected function collect_classlike(ClassLike $stmt, array $details, callable $resolve): array
     {
-        if (!array_key_exists('properties', $details)) {
-            $details['properties'] = [];
+        foreach ($stmt->getProperties() as $properties) {
+            foreach ($properties->props as $prop) {
+                $details['properties'][] = (string) $prop->name;
+            }
         }
-        $details['properties'] = array_merge(array_map(function (Property $p) {
-            return (string)$p->props[0]->name;
-        }, $stmt->getProperties()), $details['properties']);
-        $details['methods'] = array_map(function (ClassMethod $m) {
-            return (string)$m->name;
-        }, $stmt->getMethods());
-        $details['traits'] = array_map(function (TraitUse $traitUse) use ($resolve) {
-            return $resolve((string)$traitUse->traits[0]);
-        }, $stmt->getTraitUses());
+
+        foreach ($stmt->getMethods() as $method) {
+            $details['methods'][] = (string) $method->name;
+        }
+
+        foreach ($stmt->getTraitUses() as $traitUse) {
+            foreach ($traitUse->traits as $trait) {
+                $details['traits'][] = $resolve((string) $trait);
+            }
+        }
 
         return [
             $resolve($stmt->name->name) => $details,
         ];
     }
 
-    protected function collect_class(Class_ $stmt, array $uses, callable $resolve): array
+    protected function collect_class(Class_ $stmt, array $details, callable $resolve): array
     {
-        $details = [];
-
-        $details['uses'] = $uses;
-        $details['interfaces'] = array_map(function (Name $name) use ($resolve) {
-            return $resolve((string)$name);
-        }, $stmt->implements);
-        $details['enums'] = [];
+        foreach ($stmt->implements as $implement) {
+            $details['interfaces'][] = $resolve((string) $implement);
+        }
 
         // promoted properties
         if ($ctor = $stmt->getMethod('__construct')) {
@@ -133,38 +137,11 @@ class TokenScanner
         return $this->collect_classlike($stmt, $details, $resolve);
     }
 
-    protected function collect_interface(Interface_ $stmt, array $uses, callable $resolve): array
+    protected function collect_interface(Interface_ $stmt, array $details, callable $resolve): array
     {
-        $details = [];
-
-        $details['uses'] = $uses;
-        $details['interfaces'] = array_map(function (Name $name) use ($resolve) {
-            return $resolve((string)$name);
-        }, $stmt->extends);
-        $details['enums'] = [];
-
-        return $this->collect_classlike($stmt, $details, $resolve);
-    }
-
-    protected function collect_trait(Trait_ $stmt, array $uses, callable $resolve): array
-    {
-        $details = [];
-
-        $details['uses'] = $uses;
-        $details['interfaces'] = [];
-        $details['enums'] = [];
-
-        return $this->collect_classlike($stmt, $details, $resolve);
-    }
-
-    protected function collect_enum(Enum_ $stmt, array $uses, callable $resolve): array
-    {
-        $details = [];
-
-        $details['uses'] = $uses;
-        $details['interfaces'] = [];
-        $details['traits'] = [];
-        $details['enums'] = [];
+        foreach ($stmt->extends as $extend) {
+            $details['interfaces'][] = $resolve((string) $extend);
+        }
 
         return $this->collect_classlike($stmt, $details, $resolve);
     }
