b7ed5f9012dc6eaaaf1d3ba69b0e53961a607d38
DerManoMann <mano@radebatz.net>
2024-06-05 16:24:36 +1200

    Fix phpstan warning

--- src/Analysers/DocBlockParser.php
+++ src/Analysers/DocBlockParser.php
@@ -55,7 +55,7 @@ class DocBlockParser
      *
      * @param string $comment a T_DOC_COMMENT
      *
-     * @return array<OA\AbstractAnnotation>
+     * @return array<OA\AbstractAnnotation|object>
      */
     public function fromComment(string $comment, Context $context): array
     {
