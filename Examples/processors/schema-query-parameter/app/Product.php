e28255e2b9c5cdd35a51a925de3c5f12fd44789f
Martin Rademacher <mano@radebatz.net>
2024-07-15 13:23:43 +1200

    Remove annotation based `schema-query-parameter` processor example in favour of the attribute version (#1628)

--- Examples/processors/schema-query-parameter/app/Product.php
+++ Examples/processors/schema-query-parameter/app/Product.php
@@ -2,29 +2,25 @@
 
 namespace App;
 
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 
-/**
- * @OA\Schema(
- *     title="Product",
- *     description="A simple product model"
- * )
- */
+#[OA\Schema(
+    title: 'Product',
+    description: 'A simple product model',
+)]
 class Product
 {
-    /**
-     * The unique identifier of a product in our catalog.
-     *
-     * @var int
-     *
-     * @OA\Property(format="int64", example=1)
-     */
-    public $id;
-
-    /**
-     * @var string
-     *
-     * @OA\Property(format="int64", example=1)
-     */
-    public $name;
+    public function __construct(
+        #[OA\Property(
+            title: 'The unique identifier of a product in our catalog.',
+            example: 43,
+        )]
+        public int $id,
+        #[OA\Property(
+            title: 'The name of the product.',
+            example: 'Lorem ipsum',
+        )]
+        public string|null $name,
+    ) {
+    }
 }
