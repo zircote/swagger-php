56f8411318205e43fac108673a9a88277f56852b
DerManoMann <mano@radebatz.net>
2025-03-05 16:30:29 +1300

    Complete `polymorphism` example and add to examples in docs

---
+++ docs/examples/specs/polymorphism/attributes/Fl.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license Apache 2.0
+ */
+
+namespace OpenApi\Examples\Specs\Polymorphism\Attributes;
+
+use OpenApi\Attributes as OAT;
+
+#[OAT\Schema(
+    schema: 'FlResponsible'
+)]
+final class Fl extends AbstractResponsible
+{
+    public const TYPE = 'fl';
+
+    #[OAT\Property()]
+    public ?string $property3;
+}
