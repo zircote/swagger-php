7f361e623decf85aa250f31a20c69821723203a3
Radoslaw Kowalewski <git@srsbiz.pl>
2025-04-25 23:00:32 +0200

    FIX tests for windows

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -9,6 +9,7 @@ namespace OpenApi\Tests;
 use OpenApi\Annotations as OA;
 use OpenApi\Tests\Concerns\UsesExamples;
 use OpenApi\Util;
+use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Finder\Finder;
 
 class UtilTest extends OpenApiTestCase
@@ -31,13 +32,24 @@ class UtilTest extends OpenApiTestCase
         $finder = (new Finder())->in($this->examplePath('using-traits/annotations'));
         $this->assertGreaterThan(0, iterator_count($finder), 'There should be at least a few files and a directory.');
         $finder_array = \iterator_to_array($finder);
-        $directory_path = $this->examplePath('using-traits/annotations/Decoration');
-        $this->assertArrayHasKey($directory_path, $finder_array, 'The directory should be a path in the finder.');
+        $directory_path = Path::normalize($this->examplePath('using-traits/annotations/Decoration'));
+        $normalizePathKeys = function ($paths) {
+            return \array_combine(
+                \array_map(
+                    function ($path) {
+                        return Path::normalize($path);
+                    },
+                    \array_keys($paths)
+                ),
+                \array_values($paths)
+            );
+        };
+        $this->assertArrayHasKey($directory_path, $normalizePathKeys($finder_array), 'The directory should be a path in the finder.');
         // Use the Util method that should set the finder to only find files, since swagger-php only needs files.
         $finder_result = Util::finder($finder);
         $this->assertGreaterThan(0, iterator_count($finder_result), 'There should be at least a few file paths.');
         $finder_result_array = \iterator_to_array($finder_result);
-        $this->assertArrayNotHasKey($directory_path, $finder_result_array, 'The directory should not be a path in the finder.');
+        $this->assertArrayNotHasKey($directory_path, $normalizePathKeys($finder_result_array), 'The directory should not be a path in the finder.');
     }
 
     public static function shortenFixtures(): iterable
