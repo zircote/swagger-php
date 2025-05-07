eb50568d93e172b997bf57309a14a24dae527083
natsuki ueda <63272932+natsuki-engr@users.noreply.github.com>
2025-05-07 07:13:26 +0000

    replace singlequote to double quote

--- docs/snippets/guide/common-techniques/custom_property_an.php
+++ docs/snippets/guide/common-techniques/custom_property_an.php
@@ -4,15 +4,15 @@ use OpenApi\Annotations as OA;
 
 /**
  * @OA\Info(
- *   title='Example',
- *   version='1.0.0',
+ *   title="Example",
+ *   version="1.0.0",
  *   x={
- *     'some-name': 'a-value',
- *     'another': 2,
- *     'complex-type': {
- *       'supported':{
- *         {'version': '1.0', 'level': 'baseapi'},
- *         {'version': '2.1', 'level': 'fullapi'},
+ *     "some-name": "a-value",
+ *     "another": 2,
+ *     "complex-type": {
+ *       "supported":{
+ *         {"version": "1.0", "level": "baseapi"},
+ *         {"version": "2.1", "level": "fullapi"},
  *       }
  *     }
  *   }
