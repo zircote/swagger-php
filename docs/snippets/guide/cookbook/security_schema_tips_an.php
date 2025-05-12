447f406773a70e8972e9943d04a442b6fb1e9d89
natsuki ueda <63272932+natsuki-engr@users.noreply.github.com>
2025-05-12 10:25:17 +0000

    fix syntax error

--- docs/snippets/guide/cookbook/security_schema_tips_an.php
+++ docs/snippets/guide/cookbook/security_schema_tips_an.php
@@ -5,7 +5,7 @@ class Controller
     /**
      * @OA\Get(
      *   path="/api/secure/",
-     *   summary="Requires authentication"
+     *   summary="Requires authentication",
      *   security={
      *     { "api_key": {} },
      *     { "petstore_auth": {"write:pets", "read:pets"} }
