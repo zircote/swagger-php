b6ef12df533412e48e41755f34021ce744c96f93
kriss <462679766@qq.com>
2025-07-17 23:31:48 +0800

    Support use x-* in ref

--- tests/RefTest.php
+++ tests/RefTest.php
@@ -19,9 +19,23 @@ class RefTest extends OpenApiTestCase
         $this->assertInstanceOf(OA\Info::class, $info);
 
         $comment = <<<END
-@OA\Get(
+@OA\Post(
     path="/api/~/endpoint",
-    @OA\Response(response="default", description="A response")
+    @OA\Response(
+        response="default",
+        description="A response",
+        @OA\JsonContent(ref="#/components/schemas/String/x-custom-key")
+    )
+)
+
+@OA\Schema(
+     schema="String",
+     @OA\Property(property="value", type="string"),
+     x={
+        "custom-key": @OA\Schema(
+            @OA\Property(property="value", type="string"),
+        )
+    }
 )
 END;
         $openapi->merge($this->annotationsFromDocBlockParser($comment));
@@ -32,8 +46,17 @@ END;
         $analysis->validate();
         // escape / as ~1
         // escape ~ as ~0
-        $response = $openapi->ref('#/paths/~1api~1~0~1endpoint/get/responses/default');
+        $response = $openapi->ref('#/paths/~1api~1~0~1endpoint/post/responses/default');
         $this->assertInstanceOf(OA\Response::class, $response);
         $this->assertSame('A response', $response->description);
+
+        // ref x value
+        $schema = $openapi->ref('#/components/schemas/String/x-custom-key');
+        $this->assertInstanceOf(OA\Schema::class, $schema);
+
+        // ref x with deep
+        $property = $openapi->ref('#/components/schemas/String/x-custom-key/properties/value');
+        $this->assertInstanceOf(OA\Property::class, $property);
+        $this->assertSame('string', $property->type);
     }
 }
