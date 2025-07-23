e84de2da648628124f0918adcb17080ab902b686
DerManoMann <mano@radebatz.net>
2025-07-23 13:30:09 +1200

    Refactor `Util` helper into non-static `SourceFinder`

--- tests/Annotations/ComponentsTest.php
+++ tests/Annotations/ComponentsTest.php
@@ -17,4 +17,14 @@ class ComponentsTest extends OpenApiTestCase
         $this->assertEquals('#/components/schemas/bar', OA\Components::ref(new OA\Schema(['ref' => null, 'schema' => 'bar', '_context' => $this->getContext()])));
         $this->assertEquals('#/components/examples/xx', OA\Components::ref(new OA\Examples(['example' => 'xx', '_context' => $this->getContext()])));
     }
+
+    public function testRefEncode(): void
+    {
+        $this->assertSame('#/paths/~1blogs~1{blog_id}~1new~0posts', '#/paths/' . OA\Components::refEncode('/blogs/{blog_id}/new~posts'));
+    }
+
+    public function testRefDecode(): void
+    {
+        $this->assertSame('/blogs/{blog_id}/new~posts', OA\Components::refDecode('~1blogs~1{blog_id}~1new~0posts'));
+    }
 }
