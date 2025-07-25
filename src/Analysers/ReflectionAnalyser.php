5189d0c22a5b594b6efbfd6357372eb51faa2b57
DerManoMann <mano@radebatz.net>
2025-07-25 16:44:45 +1200

    Add `Context::reflector` and populate

--- src/Analysers/ReflectionAnalyser.php
+++ src/Analysers/ReflectionAnalyser.php
@@ -101,6 +101,7 @@ class ReflectionAnalyser implements AnalyserInterface
             'line' => $rc->getStartLine(),
             'annotations' => [],
             'scanned' => $details,
+            'reflector' => $rc,
         ], $analysis->context);
 
         $definition = [
@@ -131,6 +132,7 @@ class ReflectionAnalyser implements AnalyserInterface
                     'filename' => $method->getFileName() ?: null,
                     'line' => $method->getStartLine(),
                     'annotations' => [],
+                    'reflector' => $method,
                 ], $context);
                 foreach ($this->annotationFactories as $annotationFactory) {
                     $analysis->addAnnotations($annotationFactory->build($method, $ctx), $ctx);
