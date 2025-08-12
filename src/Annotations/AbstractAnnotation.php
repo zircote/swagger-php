2345b8fd638a8eae19536c76b31a165b5c971a86
DerManoMann <mano@radebatz.net>
2025-08-12 15:52:15 +1200

    CS

--- src/Annotations/AbstractAnnotation.php
+++ src/Annotations/AbstractAnnotation.php
@@ -390,7 +390,7 @@ abstract class AbstractAnnotation implements \JsonSerializable
             if (isset($data->type) && is_array($data->type)) {
                 if (in_array('null', $data->type)) {
                     $data->nullable = true;
-                    $data->type = array_filter($data->type, fn ($v) => $v !== 'null');
+                    $data->type = array_filter($data->type, fn ($v): bool => $v !== 'null');
                     if (1 === count($data->type)) {
                         $data->type = array_pop($data->type);
                     }
