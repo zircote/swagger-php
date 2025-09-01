e3ae9dabf91a8f7b8b06bd6c412171fc3a9e2372
DerManoMann <mano@radebatz.net>
2025-09-02 11:12:37 +1200

    Add `getTypeResolver()`

--- src/Processors/Concerns/TypesTrait.php
+++ src/Processors/Concerns/TypesTrait.php
@@ -7,7 +7,10 @@
 namespace OpenApi\Processors\Concerns;
 
 use OpenApi\Annotations as OA;
+use OpenApi\Context;
 use OpenApi\Generator;
+use OpenApi\Type\LegacyTypeResolver;
+use OpenApi\Type\TypeInfoTypeResolver;
 use OpenApi\TypeResolverInterface;
 
 trait TypesTrait
@@ -58,5 +61,12 @@ trait TypesTrait
             : $type;
 
         return is_array($mapped) ? $mapped[0] : $mapped;
+    }
+
+    public function getTypeResolver(?Context $context = null): TypeResolverInterface
+    {
+        return class_exists(\Radebatz\TypeInfoExtras\TypeResolver\StringTypeResolver::class)
+            ? new TypeInfoTypeResolver()
+            : new LegacyTypeResolver($context);
     }
 }
