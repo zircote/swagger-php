e25c377ec04db4d2b91186e2debaa1fb135f5cc5
Martin Rademacher <mano@radebatz.net>
2025-09-12 15:49:27 +1200

    Add response (body) scratch test (#1808)

---
+++ tests/Fixtures/Scratch/Response.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+/**
+ * @license Apache 2.0
+ */
+
+namespace OpenApi\Tests\Fixtures\Scratch;
+
+use OpenApi\Attributes as OAT;
+
+#[OAT\Info(title: 'Response', version: '1.0')]
+class ResponseController
+{
+    #[OAT\Post(
+        path: '/endpoint/response-schema',
+        responses: [
+            new OAT\Response(
+                response: 200,
+                description: 'All good',
+                content: new OAT\MediaType(
+                    mediaType: 'application/octet-stream',
+                    schema: new OAT\Schema(
+                        type: 'string',
+                        format: 'byte',
+                    ),
+                ),
+            ),
+        ]
+    )]
+    public function responseSchema()
+    {
+    }
+}
