1449b0a43d980bcbdb3ca0689a6cc77872555189
Martin Rademacher <mano@radebatz.net>
2025-11-13 16:22:50 +1300

    Merge multiple `Components` into a single one (#1836)

---
+++ tests/Fixtures/Scratch/Components.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+/**
+ * @license Apache 2.0
+ */
+
+namespace OpenApi\Tests\Fixtures\Scratch;
+
+use OpenApi\Attributes as OAT;
+
+#[OAT\Components(
+    schemas: [
+        new OAT\Schema(
+            schema: 'first schema',
+        ),
+    ],
+    requestBodies: [
+        new OAT\RequestBody(request: 'first request body'),
+    ]
+)]
+class ComponentsClass1
+{
+}
+
+#[OAT\Components(
+    schemas: [
+        new OAT\Schema(
+            schema: 'second schema',
+        ),
+    ],
+)]
+class ComponentsClass2
+{
+}
+
+#[OAT\OpenApi(
+    info: new OAT\Info(title: 'Components', version: '1.0'),
+    components: new OAT\Components(),
+)]
+#[OAT\Get(
+    path: '/endpoint',
+    responses: [
+        new OAT\Response(
+            response: 200,
+            description: 'All good',
+        ),
+    ]
+)]
+class ComponentsEndpoint
+{
+}
