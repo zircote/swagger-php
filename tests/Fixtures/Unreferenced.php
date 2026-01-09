1d481447734bf550a15452133a4c16e3d412b390
DerManoMann <mano@radebatz.net>
2026-01-09 16:22:23 +1300

    Improve `CleanUnusedComponentsTest`

--- tests/Fixtures/Unreferenced.php
+++ tests/Fixtures/Unreferenced.php
@@ -7,50 +7,59 @@
 namespace OpenApi\Tests\Fixtures;
 
 use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OAT;
 
-/**
- * @OA\Info(version="1", title="foo")
- * @OA\Schema(
- *     schema="ExampleSchema",
- *     type="object",
- * 		@OA\Property(
- *         property="layouts",
- *         type="array",
- *    		@OA\Items(
- *             type="object",
- *     			@OA\Property(
- *                 property="desktop",
- *                 ref="#/components/schemas/ExampleNestedSchema"
- *             )
- *         )
- *     )
- * )
- * @OA\Schema(
- *     schema="ExampleNestedSchema",
- *     type="object",
- *     @OA\Property(
- *         property="rows",
- *         type="array",
- *     		@OA\Items(
- *             type="object",
- *         )
- *     )
- * )
- */
+#[OAT\OpenApi(
+    info: new OAT\Info(title: 'Unreferenced', version: '1.0'),
+    security: [[ 'bearerAuth' ]],
+)]
+#[OAT\SecurityScheme(
+    securityScheme: 'bearerAuth',
+    type: 'http',
+    scheme: 'bearer',
+    description: 'Basic Auth',
+)]
+#[OAT\SecurityScheme(
+    securityScheme: 'api_key',
+    type: 'apiKey',
+    in: 'header',
+)]
+#[OAT\Schema(
+    schema: 'ExampleSchema',
+    type: 'object',
+    properties: [
+        new OAT\Property(
+            property: 'layouts',
+            items: new OAT\Items(
+                type: 'object',
+                properties: [
+                    new OAT\Property(property: 'desktop', ref: '#/components/schemas/ExampleNestedSchema'),
+                ]
+            ),
+        ),
+    ]
+)]
+#[OAT\Schema(
+    schema: 'ExampleNestedSchema',
+    type: 'object',
+    properties: [
+        new OAT\Property(property: 'rows', items: new OAT\Items(type: 'object')),
+    ]
+)]
 class Unreferenced
 {
-    /**
-     * @OA\PathItem(
-     *     path="/path",
-     * 		@OA\Parameter(
-     *         name="example_param",
-     *         in="query",
-     *         style="spaceDelimited",
-     *         explode=true,
-     *         required=true
-     *     )
-     * )
-     */
+    #[OAT\Get(
+        path: '/path',
+        parameters: [
+            new OAT\QueryParameter(
+                name: 'example_param',
+                style: 'spaceDelimited',
+                explode: true,
+                required: true,
+            ),
+        ],
+        security: [[ 'bearerAuth' ]]
+    )]
     public function someEndpoint()
     {
     }
