a9033ac3c3e315700004ef2c9501793424473512
Darius Matulionis <darius.matulionis@supermetrics.com>
2026-01-15 10:38:14 +0200

    Fix getRelativePath in SourceFinder

--- tests/SourceFinderTest.php
+++ tests/SourceFinderTest.php
@@ -21,4 +21,16 @@ class SourceFinderTest extends OpenApiTestCase
         $this->assertCount(12, $sources, 'There should be at least a few files and a directory.');
         $this->assertArrayHasKey(static::examplePath('using-traits/annotations/Decoration/Whistles.php'), $sources);
     }
+
+    public function testExclude(): void
+    {
+        $finder = (new SourceFinder(
+            static::examplePath('using-traits/annotations'),
+            static::examplePath('using-traits/annotations/Decoration')
+        ));
+        $sources = iterator_to_array($finder);
+
+        $this->assertArrayNotHasKey(static::examplePath('using-traits/annotations/Decoration/Whistles.php'), $sources);
+        $this->assertArrayNotHasKey(static::examplePath('using-traits/annotations/Decoration/Bell.php'), $sources);
+    }
 }
