82710a1f4aca7adc0734050380495ec91e5c104a
DerManoMann <mano@radebatz.net>
2026-02-20 14:33:53 +1300

    Refactor AbstractAnnotation::identity()

--- tests/Annotations/AbstractAnnotationTest.php
+++ tests/Annotations/AbstractAnnotationTest.php
@@ -7,6 +7,7 @@
 namespace OpenApi\Tests\Annotations;
 
 use OpenApi\Annotations as OA;
+use OpenApi\Annotations\Response;
 use OpenApi\Tests\OpenApiTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 
@@ -141,6 +142,21 @@ END;
         $this->assertOpenApiLogEntryContains('"example" and "examples" are mutually exclusive');
 
         $analysis->validate();
+    }
+
+    public static function identityCases(): array
+    {
+        return [
+            'default' => [new Response(['response' => 200]), null, '@OA\Response(response=200)'],
+            '[]' => [new Response(['response' => 200]), [], '@OA\Response()'],
+            'custom' => [new Response(['response' => 200]), ['response'], '@OA\Response(response=200)'],
+        ];
+    }
+
+    #[DataProvider('identityCases')]
+    public function testIdentity(OA\AbstractAnnotation $annotation, ?array $identityArgs, string $expected): void
+    {
+        $this->assertSame($expected, $annotation->identity($identityArgs));
     }
 }
 
