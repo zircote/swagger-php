54a4acc6420e762d32d04aed4cda6169fe463e7b
DerManoMann <mano@radebatz.net>
2026-07-11 19:02:11 +1200

    CS

--- tests/Processors/AugmentParametersTest.php
+++ tests/Processors/AugmentParametersTest.php
@@ -6,6 +6,7 @@
 
 namespace OpenApi\Tests\Processors;
 
+use OpenApi\Annotations\OpenApi;
 use OpenApi\Annotations\Operation;
 use OpenApi\Annotations\Parameter;
 use OpenApi\Annotations\PathItem;
@@ -32,7 +33,7 @@ final class AugmentParametersTest extends OpenApiTestCase
                 ->setTypeResolver($this->getTypeResolver()))
             ->build();
         $openapi = $result->openApi();
-        $this->assertNotNull($openapi);
+        $this->assertInstanceOf(OpenApi::class, $openapi);
         $this->assertCount(1, $openapi->components->parameters, 'OpenApi contains 1 reusable parameter specification');
         $this->assertEquals('ItemName', $openapi->components->parameters[0]->parameter, 'When no @OA\Parameter()->parameter is specified, use @OA\Parameter()->name');
     }
