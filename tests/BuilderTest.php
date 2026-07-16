4e3d1092d2d09485149d72b9fab406796cc181c2
DerManoMann <mano@radebatz.net>
2026-07-16 14:23:21 +1200

    Make all tests pass with builder mode defaulting to 'hybrid'

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -8,6 +8,7 @@ namespace OpenApi\Tests;
 
 use OpenApi\Augmenter\OperationId;
 use OpenApi\Builder;
+use OpenApi\Builder\Mode;
 use OpenApi\Generator;
 use OpenApi\Tests\Concerns\UsesExamples;
 use OpenApi\Utils\SourceFinder;
@@ -22,6 +23,7 @@ final class BuilderTest extends OpenApiTestCase
         $this->registerExampleClassloader('petstore');
 
         $result = (new Builder())
+            ->setMode(Mode::CLASSIC)
             ->addSource(self::examplePath('petstore/annotations'))
             ->setLogger($this->getTrackingLogger())
             ->withGenerator(function (Generator $generator): void {
@@ -47,6 +49,7 @@ final class BuilderTest extends OpenApiTestCase
             ->generate([$sourceDir]);
 
         $builderResult = (new Builder())
+            ->setMode(Mode::CLASSIC)
             ->addSource($sourceDir)
             ->withGenerator(function (Generator $generator): void {
                 $generator->setAnalyser($this->getAnalyzer());
@@ -114,6 +117,7 @@ final class BuilderTest extends OpenApiTestCase
         $this->assertOpenApiLogEntryContains('Required @OA\PathItem() not found');
 
         $result = (new Builder())
+            ->setMode(Mode::CLASSIC)
             ->setSources([])
             ->setLogger($this->getTrackingLogger())
             ->build();
@@ -124,6 +128,7 @@ final class BuilderTest extends OpenApiTestCase
     public function testBuildCollectsWarnings(): void
     {
         $result = (new Builder())
+            ->setMode(Mode::CLASSIC)
             ->setSources([])
             ->setLogger(new NullLogger())
             ->build();
